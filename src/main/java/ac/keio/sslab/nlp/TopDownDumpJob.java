3d1c81cb368f72b263a00ab02b6011f51c50a550
Takeshi Yoshimura <yos@sslab.ics.keio.ac.jp>
2015-07-17 11:49:15 +0900

    fix bug in TopDownDumpJob

--- src/main/java/ac/keio/sslab/nlp/TopDownDumpJob.java
+++ src/main/java/ac/keio/sslab/nlp/TopDownDumpJob.java
@@ -2,6 +2,9 @@ package ac.keio.sslab.nlp;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +16,8 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.mahout.clustering.iterator.ClusterWritable;
+import org.apache.mahout.math.Vector.Element;
 import org.apache.mahout.math.VectorWritable;
 import org.eclipse.jgit.util.FileUtils;
 
@@ -21,12 +26,11 @@ import ac.keio.sslab.nlp.lda.LDAHDFSFiles;
 import ac.keio.sslab.nlp.lda.TopicReader;
 
 public class TopDownDumpJob implements NLPJob {
-
 	NLPConf conf = NLPConf.getInstance();
 
 	@Override
 	public String getJobName() {
-		return "topDownDump";
+		return "topdownDump";
 	}
 
 	@Override
@@ -68,6 +72,12 @@ public class TopDownDumpJob implements NLPJob {
 
 			FileSystem fs = FileSystem.get(hdfsConf);
 			Path topdownPath = new Path(conf.topdownPath, args.get("t"));
+			Comparator<Entry<Integer, Double>> reverser = new Comparator<Entry<Integer, Double>>() {
+				public int compare(Entry<Integer, Double> e1, Entry<Integer, Double> e2) {
+					return e2.getValue().compareTo(e1.getValue());
+				}
+			};
+
 			for (FileStatus status: fs.listStatus(topdownPath)) {
 				Path dirPath = status.getPath();
 				if (!dirPath.getName().startsWith("topdown-")) {
@@ -83,25 +93,30 @@ public class TopDownDumpJob implements NLPJob {
 					continue;
 				}
 				Path centroidsPath = iterationSeqs[0].getPath();
-				TopicReader centroidReader = new TopicReader();
-				centroidReader.loadDictionary(topicStr);
-				centroidReader.loadTopicTermDir(centroidsPath, hdfsConf, 2);
-				for (Entry<Integer, List<String>> cluster: centroidReader.getTopics().entrySet()) {
-					sb.setLength(0);
-					for (String topic: cluster.getValue()) {
-						sb.append(',').append(topic);
+				SequenceDirectoryReader centroidReader = new SequenceDirectoryReader(centroidsPath, hdfsConf);
+				IntWritable key = new IntWritable();
+				ClusterWritable value = new ClusterWritable();
+
+				while (centroidReader.next(key, value)) {
+					Map<Integer, Double> tmpMap = new HashMap<Integer, Double>();
+					for (Element e: value.getValue().getCenter().all()) {
+						tmpMap.put(e.index(), e.get());
 					}
-					clusterStr.put(cluster.getKey(), sb.toString());
+					List<Entry<Integer, Double>> sorted = new ArrayList<Entry<Integer, Double>>(tmpMap.entrySet());
+					Collections.sort(sorted, reverser);
+					sb.setLength(0);
+					sb.append(topicStr.get(sorted.get(0).getKey())).append(':').append(sorted.get(0).getValue());
+					sb.append(',').append(topicStr.get(sorted.get(1).getKey())).append(':').append(sorted.get(1).getValue());
+					clusterStr.put(key.get(), sb.toString());
 				}
 
 				SequenceDirectoryReader seq = new SequenceDirectoryReader(clusteredPoints, hdfsConf);
-				IntWritable key = new IntWritable();
-				IntWritable value = new IntWritable();
-				while (seq.next(key, value)) {
-					if (clusterCount.containsKey(value.get())) {
-						clusterCount.put(value.get(), 0);
+				VectorWritable value2 = new VectorWritable();
+				while (seq.next(key, value2)) {
+					if (!clusterCount.containsKey(key.get())) {
+						clusterCount.put(key.get(), 0);
 					}
-					clusterCount.put(value.get(), clusterCount.get(value.get()) + 1);
+					clusterCount.put(key.get(), clusterCount.get(key.get()) + 1);
 				}
 			}
 		} catch (Exception e) {
@@ -130,6 +145,7 @@ public class TopDownDumpJob implements NLPJob {
 				sb.setLength(0);
 				int clusterID = e.getKey();
 				sb.append(clusterID).append(',').append(e.getValue()).append(',').append(clusterStr.get(clusterID));
+				pw.println(sb.toString());
 			}
 			pw.close();
 		} catch (Exception e) {
