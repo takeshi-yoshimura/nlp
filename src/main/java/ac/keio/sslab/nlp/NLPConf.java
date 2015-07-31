7a6c1f6d81ee613a654302e8685efb5aa2acbbd4
Takeshi Yoshimura <yos@sslab.ics.keio.ac.jp>
2015-07-31 16:17:13 +0900

    update tmp directory locations

--- src/main/java/ac/keio/sslab/nlp/NLPConf.java
+++ src/main/java/ac/keio/sslab/nlp/NLPConf.java
@@ -10,12 +10,12 @@ import org.codehaus.jettison.json.JSONObject;
 public class NLPConf {
 
 	public Path rootPath;
-	public File localRootFile;
 	public Path ldaPath;
 	public Path corpusPath;
 	public Path topdownPath;
 	public Path tmpPath;
 
+	public File localRootFile;
 	public File localLogFile;
 	public File localLdaFile;
 	public File localCorpusFile;
@@ -27,9 +27,9 @@ public class NLPConf {
 	public File finalStableCommitStatsFile;
 
 	public String runPath = "/var/lib/nlp/run";
-	public static String nlpDirName = "/nlp/";
+	public static String nlpHDFSDirName = "/nlp/";
 	public static String nlpLocalDirName = "/var/lib/nlp";
-	public static String tmpDirName = "/tmp/nlp/";
+	public static final String tmpDirName = "tmp";
 	public static final String logDirName = "log";
 	public static final String LDADirName = "lda";
 	public static final String tpdownDirName = "topdown";
@@ -47,20 +47,21 @@ public class NLPConf {
 	public static NLPConf getInstance() {
 		return instance;
 	}
+
 	protected void resetConf() {
-		rootPath = new Path(nlpDirName);
-		localRootFile = new File(nlpLocalDirName);
+		rootPath = new Path(nlpHDFSDirName);
 		ldaPath = new Path(rootPath, LDADirName);
 		corpusPath = new Path(rootPath, corpusDirName);
 		topdownPath = new Path(rootPath, tpdownDirName);
-		tmpPath = new Path(tmpDirName);
+		tmpPath = new Path(rootPath, tmpDirName);
 
+		localRootFile = new File(nlpLocalDirName);
 		localLogFile = new File(localRootFile, logDirName);
 		localLdaFile = new File(localRootFile, LDADirName);
 		localCorpusFile = new File(localRootFile, corpusDirName);
 		localArgFile = new File(localRootFile, argDirName);
 		localLockFile = new File(localRootFile, lockDirName);
-		localTmpFile = new File(tmpDirName);
+		localTmpFile = new File(localRootFile, tmpDirName);
 
 		finalOutputFile = new File(localRootFile, finalOutputDirName);
 		finalStableCommitStatsFile = new File(finalOutputFile, finalStableCommitStatsFileName);
@@ -70,23 +71,19 @@ public class NLPConf {
 		try {
 			FileInputStream inputStream = new FileInputStream(confFileName);
 			JSONObject jobJson = new JSONObject(IOUtils.toString(inputStream));
-			String tmpNlpDirName = nlpDirName, tmpNlpLocalDirName = nlpLocalDirName, tmpTmpDirName = tmpDirName, tmpRunPath = runPath;
-			if (jobJson.has("nlpDirName")) {
-				tmpNlpDirName = jobJson.getString("nlpDirName");
+			String tmpNlpHDFSDirName = nlpHDFSDirName, tmpNlpLocalDirName = nlpLocalDirName, tmpRunPath = runPath;
+			if (jobJson.has("nlpHDFSDirName")) {
+				tmpNlpHDFSDirName = jobJson.getString("nlpHDFSDirName");
 			}
 			if (jobJson.has("nlpLocalDirName")) {
 				tmpNlpLocalDirName = jobJson.getString("nlpLocalDirName");
-			}
-			if (jobJson.has("tmpDirName")) {
-				tmpTmpDirName = jobJson.getString("tmpDirName");
 			}
 			if (jobJson.has("runPath")) {
 				tmpRunPath = jobJson.getString("runPath");
 			}
 			inputStream.close();
-			nlpDirName = tmpNlpDirName;
+			nlpHDFSDirName = tmpNlpHDFSDirName;
 			nlpLocalDirName = tmpNlpLocalDirName;
-			tmpDirName = tmpTmpDirName;
 			runPath = tmpRunPath;
 		} catch (Exception e) {
 			System.err.println("Reading json " + confFileName + " failed: " + e.toString());
